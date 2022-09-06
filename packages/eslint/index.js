3e10dabac59f426392d81685f2ca72a8d9351801
Tristan Menzel <tristanmenzel@gmail.com>
2022-09-06 15:50:15 +1000

    Remove node config + plugin from eslint

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -1,9 +1,8 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'node', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'eslint:recommended',
-    'plugin:node/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -11,8 +10,10 @@ module.exports = {
   rules: {
     'prettier/prettier': 'warn',
     'no-console': 'warn',
-    "node/no-unsupported-features/es-syntax": "off",
-    '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true, "argsIgnorePattern": "^_", "destructuredArrayIgnorePattern": "^_"  }],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { ignoreRestSiblings: true, argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
+    ],
     'prefer-template': 'error',
   },
 }
